f02a587865b7b960e0a3c624e6e6c3525c92caf6
Nicholas Mawira <wiriama@DESKTOP-80EVKHE>
2024-09-14 12:28:19 +0300

    mukothima

--- app.py
+++ app.py
@@ -381,4 +381,4 @@ def home():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000)
